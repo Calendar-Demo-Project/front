051e66deac62f12042ce9764607d2d18f11fc1b4
Deniskakaka <krollik1000000@gmail.com>
2023-04-18 18:05:37 +0300

    fix background

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { Main } from './pages/main/Main';
 
 import style from './app.module.scss';
 import { Welcome } from './pages/welcome/Welcome';
+import { FormSingIn } from './components/formSingIn/FormSingIn';
+import { FormSingUp } from './components/formSingUp/FormSingUp';
 
 function App() {
+  const location = useLocation().search;
   return (
     <div className={style.main}>
+      {location && (
+        <div className={style.background}>
+          {location.includes('sing_in') && (
+            <FormSingIn type={'enter'} title={'Sign In to your Account'} />
+          )}
+          {location.includes('forgot_password') && (
+            <FormSingIn type="restore" title="Restore password" />
+          )}
+          {location.includes('sing_up') && (
+            <FormSingUp title="Create your Account" />
+          )}
+        </div>
+      )}
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="calendar" element={<Main />} />
